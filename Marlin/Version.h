5dc4aca970bad74f755932377f97e6c5398a81e7
fishler <marc@thecorreo.com>
2023-03-16 23:23:54 +1100

    Features

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "Fishler v1-Octoprint"
+#define SHORT_BUILD_VERSION "Fishler v1"
 
 /**
  * Verbose version identifier which should contain a reference to the location
